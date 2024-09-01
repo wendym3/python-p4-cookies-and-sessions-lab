2e590f04c6167d393d2a4a558e819a2af127df8a
wendymasese <wendy.masese@student.moringaschool.com>
2024-09-01 20:33:22 +0300

    done

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 from models import db, Article, User
 
+
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -20,15 +21,24 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+
 @app.route('/articles')
 def index_articles():
+    articles = [article for article in Article.query.all()]
+    return make_response(articles.to_dict(), 200)
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+
+    return {'message': 'Maximum pageview limit reached'}, 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -8,6 +8,7 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Article(db.Model, SerializerMixin):
     __tablename__ = 'articles'
 
